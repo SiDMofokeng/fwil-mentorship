9d00de8203e8c877e3f584da06dd352251c06678
SiDMofokeng <syd.keng@gmail.com>
2025-12-01 04:32:23 +0200

    Add AdminPanel and API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // FILE: src/App.js
 import React from 'react';
 import MentorshipForm from './components/MentorshipForm';
+import AdminPanel from './pages/AdminPanel';
 
 export default function App() {
   return <MentorshipForm />;
