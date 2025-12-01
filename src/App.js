620a2e4d57ccc206fdf9fe3169f71c3b305d14f2
SiDMofokeng <syd.keng@gmail.com>
2025-12-01 05:07:23 +0200

    Show Admin when ?admin=1 and fix filename case

--- src/App.js
+++ src/App.js
@@ -4,5 +4,8 @@ import MentorshipForm from './components/MentorshipForm';
 import AdminPanel from './pages/AdminPanel';
 
 export default function App() {
-  return <MentorshipForm />;
+  const params = new URLSearchParams(window.location.search);
+  const showAdmin = params.get('admin') === '1';
+
+  return showAdmin ? <AdminPanel /> : <MentorshipForm />;
 }
